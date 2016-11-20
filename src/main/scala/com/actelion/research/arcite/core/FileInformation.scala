73818e4b735e77880284f84dfdde6820a10e6d03
Bernard Deffarges <bernard.deffarges@gmail.com>
2016-11-20 22:58:27 +0100

    moved FileInfo and others class out of core package object

---
+++ src/main/scala/com/actelion/research/arcite/core/FileInformation.scala
@@ -0,0 +1,27 @@
+package com.actelion.research.arcite.core
+
+import java.io.File
+
+/**
+  * Created by bernitu on 20/11/16.
+  */
+case class FileInformation(name: String, fileSize: String)
+
+case class FileInformationWithSubFolder(subFolder: String, fileInformation: FileInformation)
+
+case class FileVisitor(file: File) {
+  require(file.exists())
+
+  def sizeToString(fileSize: Long): String = {
+    if (fileSize < 1024) s"$fileSize B"
+    else {
+      val z = (63 - java.lang.Long.numberOfLeadingZeros(fileSize)) / 10
+      val res = (fileSize.toDouble / (1L << (z * 10))).toInt
+      val uni = "KMGTPE" (z - 1)
+      s""" $res ${uni}B"""
+    }
+  }
+
+  lazy val fileInformation = FileInformation(file.getName, sizeToString(file.length()))
+}
+
--- src/main/scala/com/actelion/research/arcite/core/package.scala
+++ src/main/scala/com/actelion/research/arcite/core/package.scala
@@ -9,7 +9,9 @@ import org.slf4j.LoggerFactory
 /**
   * Created by deffabe1 on 7/12/16.
   */
-package object core {
+/**
+  * Created by bernitu on 20/11/16.
+  */package object core {
 
   val config = ConfigFactory.load
 
@@ -108,26 +110,4 @@ package object core {
 
     getFInfo("", subFolder)
   }
-
-  case class FileInformation(name: String, fileSize: String)
-
-  case class FileInformationWithSubFolder(subFolder: String, fileInformation: FileInformation)
-
-  case class FileVisitor(file: File) {
-    require(file.exists())
-
-    def sizeToString(fileSize: Long): String = {
-      if (fileSize < 1024) s"$fileSize B"
-      else {
-        val z = (63 - java.lang.Long.numberOfLeadingZeros(fileSize)) / 10
-        val res = (fileSize.toDouble / (1L << (z * 10))).toInt
-        val uni = "KMGTPE" (z - 1)
-        s""" $res ${uni}B"""
-      }
-    }
-
-    lazy val fileInformation = FileInformation(file.getName, sizeToString(file.length()))
-  }
-
-
 }
