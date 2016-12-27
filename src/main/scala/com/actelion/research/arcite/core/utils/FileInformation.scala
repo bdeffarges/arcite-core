9263c3d1078e433a05413ee4d4039f9859248812
Bernard Deffarges <bernard.deffarges@actelion.com>
2016-12-27 10:45:02 +0100

    typo

--- src/main/scala/com/actelion/research/arcite/core/utils/FileInformation.scala
+++ src/main/scala/com/actelion/research/arcite/core/utils/FileInformation.scala
@@ -18,7 +18,7 @@ case class FileVisitor(file: File) {
       val z = (63 - java.lang.Long.numberOfLeadingZeros(fileSize)) / 10
       val res = (fileSize.toDouble / (1L << (z * 10))).toInt
       val uni = "KMGTPE" (z - 1)
-      s""" $res ${uni}B"""
+      s"""$res ${uni}B"""
     }
   }
 
